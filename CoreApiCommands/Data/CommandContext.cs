e24869ff68c193ca98f5ff2fd3f732166863a44f
gkrajan <gkrajan@live.in>
2021-04-21 10:09:05 +0530

    Added EfCore DbContext

---
+++ CoreApiCommands/Data/CommandContext.cs
@@ -0,0 +1,20 @@
+﻿using CoreApiCommands.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiCommands.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Command> CommandItems { get; set; }
+
+    }
+}

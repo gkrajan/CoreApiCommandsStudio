22090649a56186b0d986b04deab86d27e44b963e
gkrajan <gkrajan@live.in>
2021-04-21 11:09:25 +0530

    SqlRepository Added

---
+++ CoreApiCommands/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,47 @@
+﻿using CoreApiCommands.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiCommands.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext contextDb;
+
+        public SqlCommandAPIRepo(CommandContext contextDb)
+        {
+            this.contextDb = contextDb;
+        }
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteComman(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return contextDb.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return contextDb.CommandItems.FirstOrDefault(c => c.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CoreApiCommands/Startup.cs
+++ CoreApiCommands/Startup.cs
@@ -2,6 +2,8 @@ using CoreApiCommands.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,12 +15,21 @@ namespace CoreApiCommands
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            //services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+            services.AddDbContext<CommandContext>(options=>options.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
